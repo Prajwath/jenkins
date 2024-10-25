cb319a1492470f3c8d2cb48e55c99f8be3c2c24d
prajwath <prajwathg@gmail.com>
2024-10-25 11:45:46 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ class test
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
-        System.out.println("changes made after 2 mins sucessfull in hook git");
+        System.out.println("changes made after 1 mins sucessfull in hook git");
     }
 };
